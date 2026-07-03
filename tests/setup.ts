d3682cf8f80f90d6b6e0f82f0b1bd52dfc2bb21e
Rohit-1166 <iitbombay7071@gmail.com>
2026-07-04 02:12:53 +0530

    Remove hardcoded MongoDB credentials from tests

--- tests/setup.ts
+++ tests/setup.ts
@@ -10,7 +10,7 @@ jest.setTimeout(15000);
 export const connectTestDB = async () => {
   process.env.JWT_SECRET = 'test_secret';
   process.env.JWT_EXPIRES_IN = '90d';
-  const uri = 'mongodb+srv://rohitkumar24b_db_user:Rohit1166@cluster0.vzytdbc.mongodb.net/blog-api-test?appName=Cluster0';
+  const uri = process.env.TEST_MONGO_URI || 'mongodb://localhost:27017/blog-api-test';
   await mongoose.connect(uri);
 };
 
